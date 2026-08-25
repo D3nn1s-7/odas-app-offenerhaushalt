f0b7002bfb7f036775e59f130528421eb34a763d
Dennis Liebetrau <liebetrau@ondics.de>
2026-08-25 12:35:32 +0200

    chore: proxy voll-url und shortcode-bereinigung, version gebumpt (1.23.0 -> 1.24.0)

--- app/app.js
+++ app/app.js
@@ -78,9 +78,7 @@ function getOdasAppBasePath(pathname) {
 
 function getOdasProxyEndpoint(targetUrl, pathname) {
   const appPath = getOdasAppBasePath(pathname);
-  return `${appPath}/odp-data?path=${encodeURIComponent(
-    extractPathFromUrl(targetUrl),
-  )}`;
+  return `${appPath}/odp-data?path=${encodeURIComponent(targetUrl)}`;
 }
 
 async function fetchViaOdasProxy(targetUrl) {
